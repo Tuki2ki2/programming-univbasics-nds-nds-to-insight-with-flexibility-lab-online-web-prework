2cc92ae17cccad16a24ed40274e9b23f50e53575
Erik Prenzlin <eprenzlin@gmail.com>
2020-02-17 22:55:09 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,4 +1,6 @@
 # Provided, don't edit
+require 'pry'
+
 require 'directors_database'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
@@ -29,16 +31,15 @@ def movie_with_director_name(director_name, movie_data)
     :director_name => director_name
   }
 end
-movie_data
-
-
 
 # Your code after this point
-require 'pry'
 def movies_with_director_key(name, movies_collection)
-binding.pry
+a = 0
+new_array = []
+a = movie_with_director_name(name, movie_data)
+
 end
- 
+
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -57,6 +58,22 @@ end
 
 
 def gross_per_studio(collection)
+  i = 0
+  new_hash = {}
+  gross = 0
+  while i < collection.length do
+    studio_name = collection[i][:studio]
+    gross = collection[i][:worldwide_gross]
+    if !!new_hash[studio_name]
+      new_hash[studio_name] = new_hash[studio_name] + gross
+      i+=1
+    else
+      new_hash[studio_name] = gross
+  end
+  end
+    new_hash
+    #somehow putting out 50 & 60? 
+
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
@@ -68,9 +85,37 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  binding.pry
 end
 
 def movies_with_directors_set(source)
+
+  def title(source)
+  a=0
+  method_array = []
+  while a < source.length do
+    b = 0
+    while b < source[a][:movies].length do
+      method_array << source[a][:movies][b]
+      b+=1
+  end
+  a+=1
+end
+method_array
+end
+# gives out the movies titles [{ }]
+
+def director_name(source)
+  director_name = []
+  a = 0
+  while a < source.length do
+    director_name << source[a][:name]
+    a +=1
+  end
+  director_name
+end
+
+# gives out the directors in an array
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
